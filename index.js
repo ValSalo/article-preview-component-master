c10dcd1b854f3b0dd5f8f6f30152f9518ae1d7d9
ValSalo <50966561+ValSalo@users.noreply.github.com>
2020-09-24 11:37:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$(".share-button").click(function(){
+  $(".tooltiptext").toggleClass("tooltipshow");
+  $(".share-img").toggleClass('hidden');
+  $(".share-button").toggleClass('dark');
+  $(".bottom-row").toggleClass("hide");
+  $(".box-mobile").toggleClass("show");
+});
